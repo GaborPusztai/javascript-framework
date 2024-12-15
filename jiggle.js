35ce32954a12d4a1f62bd73a09a52dcdbdf51892
GaborPusztai <156930761+GaborPusztai@users.noreply.github.com>
2024-12-15 21:11:32 +0200

    track and trigger functions modified

--- jiggle.js
+++ jiggle.js
@@ -1,4 +1,25 @@
+// global variable, dependencyMap object
+const depsMap = new Map()
+let currentEffect = null
+effectsStack = []
 
+// the (fn) variable represents the function() {fetchWeather(state.selectedCity);} function from the 'weather.js'
+// and we are creating a function called effect which will be the effect function that is then going to be saved inside of our dependencies map
+function createEffect(fn) {
+    const effect = function effect(...args) {
+        if (effectsStack.indexOf(effect) === -1) {
+            try {
+                currentEffect = effect
+                effectsStack.push(effect)
+                fn(...args)
+            } finally {
+                effectsStack.pop()
+                currentEffect = effectsStack[effectsStack.length - 1]
+            }
+        }
+    }
+    effect()
+}
 
 function render(element, content) {
     const app = document.querySelector(element)
@@ -29,13 +50,35 @@ function reactive(obj) {
         })
     })
 
+    return reactiveObj
+
+}
+
+// Tracking if we dont have any dependencies in the map object than add one
 function track(target, key) {
-    console.log(target, key)
+    if (currentEffect) {
+    let deps = depsMap.get(target)
+    if (!deps) {
+        deps = new Map()
+        depsMap.set(target, deps)
+    }
+    dep = deps.get(key)
+    if (!dep) {
+        dep = new Set()
+        deps.set(key, dep)
+    }
+    dep.add(currentEffect)
+  }
 }
 
 function trigger(target, key) {
-    console.log(target, key)
-}
-
-    return reactiveObj
+    const deps = depsMap.get(target)
+    if (!deps) return;
+    const dep = deps.get(key)
+    if (dep) {
+        const effectsToRun = new Set(dep)
+        effectsToRun.forEach(effect => {
+            effect()
+        })
+    }
 }
